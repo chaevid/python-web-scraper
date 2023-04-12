f0b0915337b6ee404f3d19f89049fc2ab8551f6c
chaevid <chaevid@gmail.com>
2023-04-12 17:59:39 +0900

    #4.5 For Loops

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-player = {
-    'name': 'chaevid',
-    'age': 31,
-    'live': True,
-    'fav_foods': ['🍗', '🍔', '🍕'],
-}
-print(player)
-print(player.pop('age'))
-print(player)
-player['xp'] = 1500
-player['fav_foods'].append('🍟')
-print(player)
-# print(player.get('fav_foods'))
+websites = ("google.com", "airbnb.com", "twitter.com",
+            "facebook.com", "youtube.com")
+
+for website in websites:
+    print(website)
