8769e97260249b5eec6f2e261396f7abffd1781d
chaevid <chaevid@gmail.com>
2023-04-12 15:41:05 +0900

    #3.3 And & Or

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-msg = "Hello World"
-print(msg)
+age = int(input("your age ?"))
+
+if age < 18:
+    print("No drink")
+elif age >= 18 & age <= 35:
+    print("You drink beer!")
+elif age == 60 | age == 70:
+    print("Birthday party!")
+else:
+    print("Go ahead")
