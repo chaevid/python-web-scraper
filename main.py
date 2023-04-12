0d082538994898d381368164073ede8bd3ca2492
chaevid <chaevid@gmail.com>
2023-04-12 17:31:30 +0900

    #4.2 Tuples

--- main.py
+++ main.py
@@ -1,11 +1,2 @@
-days_of_week = ["Mon", "Tue", "Wed", "Thu", "Fri"]
-
-print(days_of_week)
-
-days_of_week.append("Sat")
-
-print(days_of_week)
-
-days_of_week.append("Sun")
-
-print(days_of_week)
+days = ("Mon", "Tue", "Wed", "Thu", "Fri")
+print(days.index("Tue"))
