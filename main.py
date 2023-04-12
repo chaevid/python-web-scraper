eb1dc4998b5283a6de816bcd4e931643420dcf39
chaevid <chaevid@gmail.com>
2023-04-12 17:46:46 +0900

    #4.3 Dicts

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-days = ("Mon", "Tue", "Wed", "Thu", "Fri")
-print(days.index("Tue"))
+player = {
+    'name': 'chaevid',
+    'age': 31,
+    'live': True,
+    'fav_foods': ['🍗', '🍔', '🍕'],
+}
+print(player)
+print(player.pop('age'))
+print(player)
+player['xp'] = 1500
+player['fav_foods'].append('🍟')
+print(player)
+# print(player.get('fav_foods'))
