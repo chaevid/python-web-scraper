d5841b4b78d02d793fc383066c110b055c7f37aa
chaevid <chaevid@gmail.com>
2023-04-12 16:51:12 +0900

    #3.6 Python Casino

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-distance = 0
-while distance < 20:
-    print("I'm running : ", distance, "km")
-    distance += 1
-print("I'm tired :")
+from random import randint
+print("Welcome to the Python Casino!\nGuess a number between 1 and 100")
+
+pc_choice = randint(1, 100)
+user_choice = int(input(f"Choose number : "))
+
+while user_choice != pc_choice:
+    print("Lower!") if (user_choice > pc_choice) else print("Higher!")
+    # if (user_choice > pc_choice):
+    #     print("Lower!")
+    # else:
+    #     print("Higher!")
+    user_choice = int(input("Choose number :  "))
+
+print("You win!")
