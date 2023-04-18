049f3c8f2148711655c5c018eb24768b7cfe46db
chaevid <chaevid@gmail.com>
2023-04-19 01:32:54 +0900

    #5.3 Initial Request

--- main.py
+++ main.py
@@ -1,22 +1,11 @@
 from requests import get
 
-results = {}
+base_url = "https://weworkremotely.com/remote-jobs/search?&term="
+search_term = "python"
 
-websites = ("google.com", "airbnb.com", "https://twitter.com",
-            "facebook.com", "https://youtube.com")
-
-for website in websites:
-    if not website.startswith("https://"):
-        website = f"https://{website}"
-    response_code = (get(website).status_code)
-
-    if response_code >= 200 & response_code < 300:
-        results[website] = "Success"
-    elif response_code >= 300 & response_code < 400:
-        results[website] = "Redirect"
-    elif response_code >= 400 & response_code < 500:
-        results[website] = "Client Error"
-    else:
-        results[website] = "Server Error"
-
-print(results)
+response = get(f"{base_url}{search_term}")
+if response.status_code != 200:
+    print(f"Failed to get jobs. Status code: {response.status_code}")
+    # raise Exception("Failed to get jobs")
+else:
+    print(response.text)
