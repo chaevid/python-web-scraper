71c2b1c752af57c6fcd4b94b21e38ca809ff0ded
chaevid <chaevid@gmail.com>
2023-04-24 00:52:21 +0900

    #5.11 Recursive

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from requests import get
 from bs4 import BeautifulSoup
-from extractors.wwr import extract_wwr_jobs
+# from extractors.wwr import extract_wwr_jobs
+
+base_url = "https://kr.indeed.com/jobs?q="
 search_term = "python"
 
-jobs = extract_wwr_jobs(search_term)
-print(jobs)
+response = get(f"{base_url}{search_term}")
+if response.status_code != 200:
+    print("Can't request page")
+else:
+    soup = BeautifulSoup(response.text, "html.parser")
+    job_list = soup.find('ul', class_="jobsearch-ResultsList")
+    jobs = job_list.find_all("li", recursive=False)
+    for job in jobs:
+        print(jobs)
+        print("///////////")
