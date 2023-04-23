f9564a653635ea52c477ec079918770881f606ee
chaevid <chaevid@gmail.com>
2023-04-23 23:52:13 +0900

    #5.9 Recap

--- main.py
+++ main.py
@@ -18,9 +18,11 @@ else:
         for post in job_posts:
             anchors = post.find_all('a')
             anchor = anchors[1]
+            link = anchor['href']
             company, kind, region = anchor.find_all('span', class_='company')
             title = anchor.find('span', class_='title')
             job_data = {
+                'link': f"https://weworkremotely.com{link}",
                 'company': company.string,
                 'region': region.string,
                 'position': title.string,
